f603ee7e7f484e91002474d4be58bedc156d50af
Deiv-Dev <nomaxas@gmail.com>
2023-12-06 14:24:52 +0000

    made route for FindNumbersGame and helper functions

---
+++ resources/js/Helpers/ArrayHelpers.tsx
@@ -0,0 +1,39 @@
+export function shuffleArray(arrayToShuffle: number[]): number[] {
+    const shuffleArray = [...arrayToShuffle];
+    for (let i = shuffleArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffleArray[i], shuffleArray[j]] = [shuffleArray[j], shuffleArray[i]];
+    }
+    return shuffleArray;
+}
+
+export function chunkArrayToSmallerParts(
+    arrayToSplitToChunks: number[],
+    howManyChunks: number
+): number[][] {
+    return Array.from(
+        { length: Math.ceil(arrayToSplitToChunks.length / howManyChunks) },
+        (_, index) =>
+            arrayToSplitToChunks.slice(
+                index * howManyChunks,
+                (index + 1) * howManyChunks
+            )
+    );
+}
+
+export function addCurrentAnswerToArray(
+    valuesInArray: number[],
+    valueToFindInArray: number,
+    setShuffledArray: React.Dispatch<React.SetStateAction<number[]>>
+): void {
+    const numberExistsInArray = valuesInArray.includes(valueToFindInArray + 1);
+
+    if (!numberExistsInArray) {
+        const randomIndex = Math.floor(Math.random() * 9);
+        setShuffledArray((prevNumbers: number[]) => {
+            const newNumbers = [...prevNumbers];
+            newNumbers[randomIndex] = valueToFindInArray + 1;
+            return newNumbers;
+        });
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/find-numbers-game', function () {
+    return Inertia::render('Games/NumbersGames/FindNumbersGame/FindNumbersGame');
+})->name('find-numbers-game');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -35,4 +39,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
