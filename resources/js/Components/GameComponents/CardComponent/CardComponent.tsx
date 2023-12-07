7b053fefb97739c5e75bd61269dad8076d46ae3a
Deiv-Dev <nomaxas@gmail.com>
2023-12-07 10:23:32 +0000

    add animation on correct or wrong card press

---
+++ resources/js/Components/GameComponents/CardComponent/CardComponent.tsx
@@ -0,0 +1,45 @@
+import { Row, Col } from "react-bootstrap";
+import "./CardComponentsStyle.scss";
+
+interface CardComponentsProps {
+    chunkedArray: number[][];
+    cardsBackgroundColors: string[];
+    shuffledNumbersToShowOnCards: number[];
+    handleCardClick: (card: number) => void;
+}
+
+const CardComponents = ({
+    chunkedArray,
+    cardsBackgroundColors,
+    shuffledNumbersToShowOnCards,
+    handleCardClick,
+}: CardComponentsProps) => {
+    return (
+        <>
+            {chunkedArray.map((row: number[], rowIndex: number) => (
+                <Row key={`row-${row}`}>
+                    {row.map((card: number, colIndex: number) => (
+                        <Col
+                            style={{
+                                backgroundColor:
+                                    cardsBackgroundColors[
+                                        Math.floor(
+                                            Math.random() *
+                                                shuffledNumbersToShowOnCards.length
+                                        )
+                                    ],
+                            }}
+                            className="card"
+                            key={`col-${row}-${card}`}
+                            onClick={() => handleCardClick(card)}
+                        >
+                            <p className="card__text">{card}</p>
+                        </Col>
+                    ))}
+                </Row>
+            ))}
+        </>
+    );
+};
+
+export default CardComponents;
---
+++ resources/js/Components/GameComponents/FeedbackMessageComponent/FeedbackMessageComponent.tsx
@@ -0,0 +1,26 @@
+import wrong from "../../../Images/wrong.svg";
+import correct from "../../../Images/correct.svg";
+import "./FeedbackMessageComponentStyles.scss";
+
+interface FeedbackMessageComponentProps {
+    isCorrect: boolean | null;
+}
+
+const FeedbackMessageComponent = ({
+    isCorrect,
+}: FeedbackMessageComponentProps) => {
+    return (
+        <div className="feedback__message">
+            {isCorrect === true && <img src={correct} alt="correct" />}
+            {isCorrect === false && (
+                <img
+                    className="feedback__message__wrong"
+                    src={wrong}
+                    alt="wrong"
+                />
+            )}
+        </div>
+    );
+};
+
+export default FeedbackMessageComponent;
